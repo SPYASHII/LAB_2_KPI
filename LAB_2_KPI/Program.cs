aa10388f7ff1a0502189358a4cd04c8778598022
SPYASHII <nikitaklime2005@gmail.com>
2023-03-19 23:55:25 +0200

    Добавьте файлы проекта.

---
+++ LAB_2_KPI/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace LAB_2_KPI
+{
+    public class DinArr
+    {
+        private Random rand = new Random();
+        public int[] arr { get; }
+        public int length { get; }
+        public DinArr(int length)
+        {
+            this.arr = new int[length];
+            this.length = length;
+        }
+
+        [JsonConstructor]
+        public DinArr(int[] arr)
+        {
+            this.arr = arr;
+            length = arr.Length;
+        }
+        ~DinArr()
+        {
+            Console.WriteLine("What am i exist for?");
+        }
+        public void FillRandom()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = rand.Next(1, 10);
+            }
+        }
+        public void Randomize()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                int randNum = rand.Next(0, length - 1);
+                int num = arr[randNum];
+                arr[randNum] = arr[i];
+                arr[i] = num;
+            }
+        }
+        public int FindNumOf(int value)
+        {
+            int num = arr.Count(a => a == value);
+            return num;
+        }
+        public void ShowArr()
+        {
+            foreach(var i in arr)
+            {
+                Console.Write(i + "|");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var a = new DinArr(10);
+
+            a.FillRandom();
+            a.ShowArr();
+            Console.WriteLine("\n" + a.FindNumOf(5));
+            a.Randomize();
+            a.ShowArr();
+            Console.WriteLine("\n" + a.FindNumOf(5));
+            
+            SaveJson(a);
+
+            try
+            {
+                var b = LoadJson();
+
+                Console.WriteLine($"Arr b length: \n{b.length}\n");
+                b.ShowArr();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Some error with Json file");
+            }
+            Console.ReadKey();
+        }
+        static void SaveJson(DinArr a)
+        {
+            string json = JsonSerializer.Serialize(a);
+            File.WriteAllText("json.json", json);
+        }
+        static DinArr LoadJson()
+        {
+            string json = File.ReadAllText("json.json");
+            DinArr b = JsonSerializer.Deserialize<DinArr>(json);
+            return b;
+        }
+    }
+}
